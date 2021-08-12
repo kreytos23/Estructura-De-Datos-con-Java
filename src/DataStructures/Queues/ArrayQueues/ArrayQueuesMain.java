f3a5717355af26348dfe841c77132b5abbeae66c
Cesar Martin <kreytos.pavi10@gmail.com>
2021-08-12 18:56:45 -0500

    Queues with Array

---
+++ src/DataStructures/Queues/ArrayQueues/ArrayQueuesMain.java
@@ -0,0 +1,35 @@
+package DataStructures.Queues.ArrayQueues;
+
+public class ArrayQueuesMain {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayQueues queue = new ArrayQueues(10);
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.add(marySmith);
+        queue.add(mikeWilson);
+        queue.add(billEnd);
+        //queue.printQueue();
+
+        queue.remove();
+        queue.remove();
+        //queue.printQueue();
+
+        //System.out.println(queue.peek());
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        //queue.remove();
+
+        queue.add(mikeWilson);
+
+
+        queue.printQueue();
+
+    }
+}
